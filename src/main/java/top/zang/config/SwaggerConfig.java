03ca3ca7e97e646b0852d4d0b320f536eeca1fde
zanghongmin <zanghongmin@aixin-ins.com>
2021-08-23 14:32:45 +0800

    swagger 增加权限控制 方便使用

--- src/main/java/top/zang/config/SwaggerConfig.java
+++ src/main/java/top/zang/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package top.zang.config;
 
+import springfox.documentation.service.*;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import top.zang.config.token.MyToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,10 +10,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ScalarType;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ParameterType;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.util.Collections.singletonList;
 
@@ -28,20 +31,49 @@ public class SwaggerConfig {
 
     @Bean
     public Docket buildDocket(){
-        return new Docket(DocumentationType.OAS_30)
+        Docket docket =  new Docket(DocumentationType.OAS_30)
                 .enable(swaggerEnable)
                 .apiInfo(buildApiInf())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(web_package))
                 .paths(PathSelectors.any())
-                .build()
-                .globalRequestParameters(singletonList(new springfox.documentation.builders.RequestParameterBuilder()
-                        .name(MyToken.Authorization_TOKEN)
-                        .description("令牌token")
-                        .in(ParameterType.HEADER)
-                        .required(false)
-                        .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
-                        .build()));
+                .build();
+            if (swaggerEnable) {
+                docket.securitySchemes(securitySchemes()).securityContexts(securityContexts());
+            }
+        return docket;
+    }
+
+
+    private List<SecurityScheme> securitySchemes() {
+        //设置请求头信息
+        List<SecurityScheme> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey(MyToken.Authorization_TOKEN, MyToken.Authorization_TOKEN, "header");
+        result.add(apiKey);
+        return result;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        //设置需要登录认证的路径
+        List<SecurityContext> result = new ArrayList<>();
+        result.add(getContextByPath("/*/.*"));
+        return result;
+    }
+
+    private SecurityContext getContextByPath(String pathRegex) {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(pathRegex))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> result = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        result.add(new SecurityReference(MyToken.Authorization_TOKEN, authorizationScopes));
+        return result;
     }
 
     private ApiInfo buildApiInf(){
